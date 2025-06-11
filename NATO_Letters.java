53343f62f26e24704bc1d0bf155c9a8d3014f3c1
Mohd Naved Sheikh <navedsheikh7983@gmail.com>
2025-06-12 02:12:37 +0530

    Create NATO_Letters.java

---
+++ NATO_Letters.java
@@ -0,0 +1,88 @@
+public class NaToClass {
+    public static void main(String[] args) {
+        char letter='Z';
+        switch (letter){
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            case 'F':
+                System.out.println("Fog");
+                break;
+            case 'G':
+                System.out.println("George");
+                break;
+            case 'H':
+                System.out.println("How");
+                break;
+            case 'I':
+                System.out.println("Item");
+                break;
+            case 'J':
+                System.out.println("jig");
+                break;
+            case 'K':
+                System.out.println("King");
+                break;
+            case 'L':
+                System.out.println("Love");
+                break;
+            case 'M':
+                System.out.println("Mike");
+                break;
+            case 'N':
+                System.out.println("Nane");
+                break;
+            case 'O':
+                System.out.println("Oboe");
+                break;
+            case 'P':
+                System.out.println("Peter");
+                break;
+            case 'Q':
+                System.out.println("Queen");
+                break;
+            case 'R':
+                System.out.println("Roger");
+                break;
+            case 'S':
+                System.out.println("Suger");
+                break;
+            case 'T':
+                System.out.println("Tare");
+                break;
+            case 'U':
+                System.out.println("Uncle");
+                break;
+            case 'V':
+                System.out.println("Victor");
+                break;
+            case 'W':
+                System.out.println("William");
+                break;
+            case 'X':
+                System.out.println("X-Ray");
+                break;
+            case 'Y':
+                System.out.println("Yoke");
+                break;
+            case 'Z':
+                System.out.println("Zebra");
+                break;
+
+        }
+
+    }
+
+}
