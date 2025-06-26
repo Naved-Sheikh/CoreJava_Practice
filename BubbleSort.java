76234fea41c01091b2807f7b99adb7cdae343445
Mohd Naved Sheikh <navedsheikh7983@gmail.com>
2025-06-27 00:26:40 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,35 @@
+public class BubbleSortExample {
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        printArray(numbers);
+
+        bubbleSort(numbers);
+
+        System.out.println("Sorted array using Bubble Sort:");
+        printArray(numbers);
+    }
+}
